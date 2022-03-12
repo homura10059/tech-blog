3782d354599260d60243a06f059916d8a316177b
o-hayato <preasper0+github@gmail.com>
2022-03-12 21:39:45 +0900

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+const config = {
+    "semi": false,
+    "singleQuote": true,
+    "trailingComma": "none",
+    "arrowParens": "avoid"
+};
+
+module.exports = config
