1f46a7aadc9726ab91c1bc24e950848cc3e00384
o-hayato <preasper0+github@gmail.com>
2022-03-13 13:37:38 +0900

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
 import '../styles/index.css'
 
 import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+import { GA_TRACKING_ID, pageview } from '../lib/gtag'
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter()
+  useEffect(() => {
+    // GA_TRACKING_ID が設定されていない場合は、処理終了
+    if (!GA_TRACKING_ID) return
+
+    const handleRouteChange = (url: string) => {
+      pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
