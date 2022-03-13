7530bd45476d4a4526b7ce5c2227afc8533be0a6
o-hayato <preasper0+github@gmail.com>
2022-03-13 13:01:06 +0900

    move app code path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+/** @type {import('@types/tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
-  content: ['./components/**/*.tsx', './pages/**/*.tsx'],
+  mode: 'jit',
+  content: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
   theme: {
     extend: {},
   },
