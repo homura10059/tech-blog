b933e0c5e6761fe216a95a8d4dbbb59cc917975e
o-hayato <preasper0+github@gmail.com>
2022-03-18 20:54:23 +0900

    add markdown style

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,7 +11,7 @@ const Layout = ({ preview, children }: Props) => {
   return (
     <>
       <Meta />
-      <div className="min-h-screen">
+      <div className="min-h-screen bg-background-dark text-surface">
         <Alert preview={preview} />
         <main>{children}</main>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   mode: 'jit',
   content: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: {
+          dark: '#2B2D42',
+          light: '#8D99AE'
+        },
+        surface: '#EDF2F4',
+        primary: {
+          light: '#EF233C',
+          dark: '#D90429'
+        }
+      }
+    }
   },
-  plugins: []
+  plugins: [require('@tailwindcss/typography')]
 }
