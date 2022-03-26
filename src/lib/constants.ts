9673d55394c6e85d5cbe3fc9ee2f8adddab73c78
o-hayato <preasper0+github@gmail.com>
2022-03-26 21:07:56 +0900

    add post

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const EXAMPLE_PATH = 'blog-starter-typescript'
-export const BLOG_TITLE = "o-hayato's tech blog"
+export const BLOG_TITLE = "homura's tech blog"
 export const STORY_BOOK_URL = 'https://o-hayato.github.io/tech-blog/storybook/'
