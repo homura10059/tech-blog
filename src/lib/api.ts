02900b26597b95811ea3327c7634dc43d7e458a3
o-hayato <preasper0+github@gmail.com>
2022-04-07 21:41:59 +0900

    add medium posts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -43,6 +43,7 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
 export function getAllPosts(fields: string[] = []) {
   const slugs = getPostSlugs()
   return slugs
+    .filter(slug => slug !== '.DS_Store')
     .map(slug => getPostBySlug(slug, fields))
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
 }
