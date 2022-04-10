d20e90b5e19578bda6bbca39e7166322e8042777
o-hayato <preasper0+github@gmail.com>
2022-04-10 12:22:42 +0900

    remove unused

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,6 +1,5 @@
 import { differenceInYears, parseISO } from 'date-fns'
 import ErrorPage from 'next/error'
-import Head from 'next/head'
 import { useRouter } from 'next/router'
 
 import Warning from '../../components/domain/banners/warning'
