2d3089735a16eacae0517ff10c5b334f3db88529
o-hayato <preasper0+github@gmail.com>
2022-04-23 13:39:33 +0900

    fix width

--- src/components/domain/banners/warning.tsx
+++ src/components/domain/banners/warning.tsx
@@ -5,7 +5,7 @@ type Props = {
 }
 const Warning: React.VFC<Props> = ({ text }) => {
   return (
-    <div className="bg-yellow-300 max-w-7xl mx-auto py-1 px-1 sm:py-2 sm:px-3 lg:px-4">
+    <div className="bg-yellow-300 max-w-7xl mx-auto py-1 px-1 sm:py-2 sm:px-3 lg:px-4 min-w-full">
       <div className="flex items-center justify-between flex-wrap">
         <div className="w-0 flex-1 flex items-center justify-center">
           <ExclamationIcon
