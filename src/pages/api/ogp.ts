9f25423cb8954e84518b4f5d1dfb6afc02d4bd6b
o-hayato <preasper0+github@gmail.com>
2022-05-07 12:56:06 +0900

    fix api

--- src/pages/api/ogp.ts
+++ src/pages/api/ogp.ts
@@ -4,9 +4,9 @@ import { getOgp } from '../../lib/getOgp'
 
 const ogp = async (req: NextApiRequest, res: NextApiResponse) => {
   const url = req.query.url
-  if (!url || Array.isArray(url)) return res.status(400)
+  if (!url || Array.isArray(url)) return res.status(400).end()
   const meta = await getOgp(url)
-  res.status(200).json(meta)
+  return res.status(200).json(meta)
 }
 
 export default ogp
