7bae58891142b6b1fcbf689abdff3d8e812edff1
homura <preasper0+github@gmail.com>
2022-05-29 13:12:27 +0900

    fix link title

--- src/components/domain/embed/rich-link.tsx
+++ src/components/domain/embed/rich-link.tsx
@@ -19,7 +19,7 @@ const RichLink: VFC<Props> = ({ url, title, description, image }) => {
         <a className="">
           <div className="flex w-full border border-primary-light items-center divide-x divide-primary-light justify-between hover:opacity-50">
             <div className="text-surface flex flex-col gap-1 px-2">
-              <span className="font-bold">{title}</span>
+              <span className="font-bold line-clamp-1">{title}</span>
               <span className="text-xs text-background-light line-clamp-1">
                 {description}
               </span>
