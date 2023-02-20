b14c9ceea6c698c5569a0205e66b9f5771be724c
homura <preasper0+github@gmail.com>
2023-02-20 21:11:42 +0900

    add story

--- src/components/domain/card/card.stories.tsx
+++ src/components/domain/card/card.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import React from 'react'
 
-import { Card } from './card'
+import Card from './card'
 
 export default {
   title: 'components/domain/card',
@@ -17,15 +17,17 @@ const Template: ComponentStory<typeof Card> = args => (
 
 export const Default = Template.bind({})
 Default.args = {
-  title: 'EdgeRouter X',
-  description:
-    '宅内の routing とインターネット接続を一挙に受け持つルーター。無線機のは無いので別の機器を無線APとしている',
-  imageUrl:
-    'https://images-na.ssl-images-amazon.com/images/P/B010MZFH5A.09.LZZZZZZZ.jpg',
-  linkUrl: 'https://www.amazon.co.jp/dp/B010MZFH5A?tag=homura10059-22'
+  meta: {
+    title: 'EdgeRouter X',
+    description:
+      '宅内の routing とインターネット接続を一挙に受け持つルーター。無線機のは無いので別の機器を無線APとしている',
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/P/B010MZFH5A.09.LZZZZZZZ.jpg',
+    linkUrl: 'https://www.amazon.co.jp/dp/B010MZFH5A?tag=homura10059-22'
+  }
 }
 
-// export const Loading = Template.bind({})
-// Loading.args = {
-//   isLoading: true
-// }
+export const Loading = Template.bind({})
+Loading.args = {
+  isLoading: true
+}
--- src/components/domain/card/card.tsx
+++ src/components/domain/card/card.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { VFC } from 'react'
 
 import { customLoader } from '../../../lib/image-loader'
 
@@ -12,7 +13,8 @@ export type CardProps = {
 
 export const Card = ({ imageUrl, linkUrl, title, description }: CardProps) => {
   return (
-    <Link href={linkUrl}>
+    // eslint-disable-next-line tailwindcss/no-custom-classname
+    <Link href={linkUrl} className="not-prose">
       {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
       <div className="flex flex-row place-items-center bg-white rounded-lg shadow-lg shadow-surface hover:cursor-pointer @container">
         <figure className="relative w-24 @sm:w-48 h-24 @sm:h-48">
@@ -35,3 +37,36 @@ export const Card = ({ imageUrl, linkUrl, title, description }: CardProps) => {
     </Link>
   )
 }
+
+export const SkeletonLoader = () => {
+  return (
+    // eslint-disable-next-line tailwindcss/no-custom-classname
+    <div className="not-prose">
+      {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
+      <div className="flex flex-row place-items-center bg-white rounded-lg shadow-lg shadow-surface hover:cursor-pointer @container">
+        <div className="relative w-24 @sm:w-48 h-24 @sm:h-48 bg-slate-300 rounded animate-pulse"></div>
+        <div className="grid flex-1 grid-cols-3 p-3 @sm:p-6 animate-pulse">
+          <div className="col-span-2 h-6 bg-slate-300 rounded"></div>
+          <div className="col-span-3 mt-2 h-4 bg-slate-300 rounded"></div>
+          <div className="col-span-3 mt-2 h-4 bg-slate-300 rounded"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+type ConnectProps =
+  | {
+      meta?: undefined
+      isLoading: true
+    }
+  | {
+      meta: CardProps
+      isLoading?: undefined
+    }
+const Connect: VFC<ConnectProps> = ({ meta, isLoading }) => {
+  if (isLoading) return <SkeletonLoader />
+  return <Card {...meta} />
+}
+
+export default Connect
