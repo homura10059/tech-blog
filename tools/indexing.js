f8aa39a91a3febc3cdd687cbab60e40c342280a4
homura <preasper0+github@gmail.com>
2023-03-21 20:54:34 +0900

    update tool

--- tools/indexing.js
+++ tools/indexing.js
@@ -1,6 +1,8 @@
-var { google } = require('googleapis')
-var axios = require('axios')
-var key = require('./service_account.json')
+const fs = require('fs')
+const { join } = require('path')
+const { google } = require('googleapis')
+const axios = require('axios')
+const key = require('./service_account.json')
 
 const jwtClient = new google.auth.JWT(
   key.client_email,
@@ -10,15 +12,25 @@ const jwtClient = new google.auth.JWT(
   null
 )
 
+const postsDirectory = join(process.cwd(), '_posts')
+
+const getUrls = () => {
+  return fs
+    .readdirSync(postsDirectory, { withFileTypes: true })
+    .filter(x => x.isFile())
+    .map(x => x.name.replace(/\.md$/, ''))
+    .filter(slug => slug !== '.DS_Store')
+    .filter(slug => !slug.startsWith('draft'))
+    .map(slug => `https://tech-blog.homura10059.dev/posts/${slug}`)
+}
+
 jwtClient.authorize(async function (err, tokens) {
   if (err) {
     console.log(err)
     return
   }
-  const urls = [
-    // TODO: ここにindexに追加したいURLをいれる
-  ]
-  await Promise.all(
+  const urls = getUrls()
+  const results = await Promise.allSettled(
     urls.map(url => {
       const options = {
         url: 'https://indexing.googleapis.com/v3/urlNotifications:publish',
@@ -32,9 +44,14 @@ jwtClient.authorize(async function (err, tokens) {
           type: 'URL_UPDATED'
         }
       }
-      return axios
-        .request(options)
-        .then(res => console.log(JSON.stringify(res.data)))
+      return axios.request(options)
     })
   )
+  const fulfilled = results
+    .filter(result => result.status === 'fulfilled')
+    .map(result => result.value.data)
+  console.log(fulfilled)
+
+  const rejected = results.filter(result => result.status === 'rejected')
+  console.log(rejected)
 })
