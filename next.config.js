b9a9284cc2f924d00c5847e137001c564621efb1
homura <preasper0+github@gmail.com>
2023-05-02 10:33:42 +0900

    fix warn

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@ module.exports = {
   images: {
     loader: 'custom',
     domains: ['i.imgur.com']
+  },
+  webpack(config) {
+    config.infrastructureLogging = { debug: /PackFileCache/ }
+    return config
   }
 }
