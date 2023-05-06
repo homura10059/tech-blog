36edc45b48a2984c30c00a79cd5d183a56b958c1
homura <preasper0+github@gmail.com>
2023-05-06 20:22:29 +0900

    use hash

--- src/components/domain/tags/tag.tsx
+++ src/components/domain/tags/tag.tsx
@@ -1,3 +1,4 @@
+import { createHash } from 'crypto'
 import Link from 'next/link'
 
 type Props = {
@@ -5,10 +6,12 @@ type Props = {
 }
 
 const Tag = ({ text }: Props) => {
+  const md5 = createHash('md5')
+  const hash = md5.update(text, 'binary').digest('hex')
   return (
-    <Link href={`/tags/${text}`}>
+    <Link href={`/tags/${hash}`}>
       <a>
-        <span className="rounded-md bg-white/60 py-1 px-2 text-black before:mr-0.5 before:content-['#'] hover:bg-white/80">
+        <span className="rounded-md bg-white/60 px-2 py-1 text-black before:mr-0.5 before:content-['#'] hover:bg-white/80">
           {text}
         </span>
       </a>
