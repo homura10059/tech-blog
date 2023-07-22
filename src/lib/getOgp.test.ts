8a87c940fc29a0cbafbfeef63b2d65116b31a0ea
homura <preasper0+github@gmail.com>
2023-07-22 20:13:16 +0900

    add tests

--- src/lib/getOgp.test.ts
+++ src/lib/getOgp.test.ts
@@ -2,12 +2,14 @@ import { expect, test } from 'vitest'
 
 import { getOgp } from './getOgp'
 
-test.each([
-  'https://tech-blog.homura10059.dev/',
-  'https://tech-blog.homura10059.dev/posts/2022-04-23-1',
-  'https://www.amazon.co.jp/dp/B09RQGMYKZ/',
-  'https://www.bbiq.jp/'
-])('getOgp', async url => {
+test.each`
+  url
+  ${'https://tech-blog.homura10059.dev/'}
+  ${'https://tech-blog.homura10059.dev/posts/2022-04-23-1'}
+  ${'https://www.amazon.co.jp/dp/B09RQGMYKZ/'}
+  ${'https://www.bbiq.jp/'}
+  ${'https://preducts.jp/products/mount-switch'}
+`('getOgp for $url', async ({ url }) => {
   const actual = await getOgp(url)
   expect(actual).toMatchSnapshot()
 })
