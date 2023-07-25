f8febc7f5f7ac6dbfc09a85c12064c18b3be09ae
homura <preasper0+github@gmail.com>
2023-07-25 21:40:51 +0900

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,15 +4,38 @@ import 'highlight.js/styles/monokai.css'
 import { Metadata } from 'next'
 
 import Analytics from '../components/client/scripts/google-analytics'
-import { BLOG_DESCRIPTION, BLOG_TITLE } from '../lib/constants'
+import { BLOG_DESCRIPTION, BLOG_TITLE, TWITTER_ACCOUNT } from '../lib/constants'
 
 export const metadata: Metadata = {
-  title: BLOG_TITLE,
+  title: {
+    template: `%s | ${BLOG_TITLE}`,
+    default: BLOG_TITLE
+  },
   description: BLOG_DESCRIPTION,
   icons: {
     icon: '/favicon/favicon-32x32.png',
     apple: '/favicon/apple-touch-icon.png',
     shortcut: '/favicon/favicon.ico'
+  },
+  manifest: '/favicon/site.webmanifest',
+  alternates: {
+    types: {
+      'application/rss+xml': '/rss/feed.xml'
+    }
+  },
+  openGraph: {
+    type: 'website',
+    title: {
+      template: `%s | ${BLOG_TITLE}`,
+      default: BLOG_TITLE
+    },
+    description: BLOG_DESCRIPTION,
+    siteName: BLOG_TITLE,
+    images: ['https://i.imgur.com/BqDJIrtm.png']
+  },
+  twitter: {
+    card: 'summary',
+    site: TWITTER_ACCOUNT
   }
 }
 export default function RootLayout({
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,8 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Profile'
+}
+export default async function Page() {
+  return <div>Profile</div>
+}
