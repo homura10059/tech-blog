9b85b3216b9c5581290f7b2775a7cc11c73f7ec9
homura <preasper0+github@gmail.com>
2023-07-26 21:27:51 +0900

    move page

---
+++ src/components/page/about.tsx
@@ -0,0 +1,40 @@
+import PageTitle from '../server/headless/Headding/page-title'
+import SectionTitle from '../server/headless/Headding/section-title'
+import LinkText from '../server/headless/link-text'
+
+type Props = {}
+
+const About = ({}: Props) => {
+  return (
+    <div className="mx-auto mt-4 max-w-2xl">
+      <PageTitle>このブログについて</PageTitle>
+      <SectionTitle>概要</SectionTitle>
+      <p>
+        このブログは個人の見解に基づいて書かれており、所属する会社、組織とは全く関係ありません
+      </p>
+      <SectionTitle>技術スタック</SectionTitle>
+      <p>
+        <LinkText
+          href={
+            'https://github.com/vercel/next.js/tree/canary/examples/blog-starter-typescript'
+          }
+          className={'mr-1'}
+        >
+          blog-starter-typescript
+        </LinkText>{' '}
+        を基に Next.js & TypeScript & tailwindcss を使って作られています
+      </p>
+      <SectionTitle>リンク</SectionTitle>
+      <ul className="list-inside list-disc">
+        <li>
+          <LinkText href={'/about/privacy_policy'}>Privacy Policy</LinkText>
+        </li>
+        <li>
+          <LinkText href={'/about/disclaimer'}>Disclaimer</LinkText>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default About
