22f7b92a04878fbb806a24b0c2d965a5f5dffa21
homura <preasper0+github@gmail.com>
2023-10-16 19:55:12 +0900

    fix image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     },
     description: BLOG_DESCRIPTION,
     siteName: BLOG_TITLE,
-    images: ['https://i.imgur.com/H2X0iCp.png']
+    images: ['https://i.imgur.com/oZCMyWE.png']
   },
   twitter: {
     card: 'summary',
